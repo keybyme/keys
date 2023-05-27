4a63b3698ce0595782c43c8124fcf8ba74fcd0e7
keybyme <wesnetwork@keybyme.com>
2023-05-27 11:37:42 -0400

    version3.0

--- claves/urls.py
+++ claves/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="nombrex"),
     path('cats/', views.cats, name="catsx"),
     path('items/', views.items, name="itemsx"),
-    path('login/', views.login_user, name="login")
+    path('login/', views.login_user, name="login"),
+    path('edit_item/<int:pk>', views.edit_items, name="editx")
 ]
--- claves/views.py
+++ claves/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Categoria, Item
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from .forms import ItemForm
+
+
+
 @login_required(login_url='/login/')
 def index(request):
     return render(request, 'main.html')
@@ -15,6 +19,16 @@ def cats(request):
 def items(request):
     tran=Item.objects.all()
     return render(request, 'items.html', {"itemkey":tran})
+
+@login_required(login_url='/login/')
+def edit_items(request, pk):
+    tran=Item.objects.get(id=pk)
+    form=ItemForm(request.POST or None, instance=tran)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('itemsx')
+    return render(request, 'edit_item.html', {"form":form})
 
 def login_user(request):
     if request.user.is_authenticated:
